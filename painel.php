7df5fdf189a4ffb3db68065a6a2fed37094c863d
Matheus Viana <matheusvcv@yahoo.com.br>
2022-08-04 19:37:04 -0300

    Atualizado interface inicial do usuário.

--- painel.php
+++ painel.php
@@ -1,17 +1,28 @@
 <?php
 
 	require "protect.php";
-
-	/*if(!isset($_SESSION)){
-		session_start();
-	}*/
-
-
- 		echo "Peidou na sala!";
-
- 		echo $_SESSION['nome'];
-
-
  ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" width="device-width initial-scale=1.0">
+	<title>Aluguel de veículos</title>
+</head>
+<body>
  <a href="logout.php">Sair</a>
+ 	<h1>Aluguel de veículos</h1>
+ 		<?php echo "Seja bem vindo, colaborador " . strtoupper($_SESSION['nome']) . " o que deseja fazer?"; ?>
+ 		
+ 		<p>
+ 		<label><a href="">Cadastrar um novo Cliente:</a></label>
+ 		</p>
+ 		<p>
+ 			<label><a href="">Consultar o banco de clientes</a></label>
+ 		</p>
+
+
+</body>
+</html>
+
