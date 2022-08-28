a2b3e8cffc6ed80861a38dc99ed4744a02e35049
Matheus Viana <matheusvcv@yahoo.com.br>
2022-08-28 11:22:38 -0300

    Desenvolvida feature que exibe o valor de locação com base no grupo de veículo específico

--- resultado.php
+++ resultado.php
@@ -13,15 +13,49 @@
 	<div id="container">
 
 		<p>Você irá retirar o veículo na cidade de: <?php echo $_POST['local_retirada']; ?></p>
-		<p>Na data de <?php echo $_POST['data_retirada']; ?></p>
+		<p>Na data de <?php $data = strtotime($_POST['data_retirada']); echo date('d/m/y H:i:s', $data); ?></p>
 		<p>O veículo será pertencente ao grupo: <?php echo $_POST['grupo']; ?></p>
 		<p>E você permanecerá com o veículo pelo período de <?php echo $_POST['periodo']; ?></p>
 
 		<h2>Informações de valores:</h2>
+
+			<?php
+
+				if($_POST['grupo'] === 'A'){
+
+					echo "Você escolheu um carro do <strong> Grupo " . $_POST['grupo'] . "</strong>. Carros deste grupo custam <strong>R$ 100,00</strong> por diária.";
+
+				} else if($_POST['grupo'] === 'B') {
+
+					echo "Você escolheu um carro do <strong> Grupo " . $_POST['grupo'] . "</strong>. Carros deste grupo custam <strong>R$ 150,00</strong> por diária.";
+
+				} else if ($_POST['grupo'] === 'C'){
+
+					echo "Você escolheu um carro do <strong> Grupo " . $_POST['grupo'] . "</strong>. Carros deste grupo custam <strong>R$ 200,00</strong> por diária.";
+				}
+
+
+				if($_POST['periodo']){
+
+				}
+
+
+			?>
+
+
+
+		<!-- 
+
+
+
+
+
+
+
 		<p><strong>Tipos de carro:</strong></p>
 		<p>Tipo A = R$ 100,00 por Diária</p>
 		<p>Tipo B = R$ 150,00 por Diária</p>
-		<p>Tipo C = R$ 200,00 por Diária</p>
+		<p>Tipo C = R$ 200,00 por Diária</p> -->
 
 
 			<div id="logo">
@@ -31,4 +65,4 @@
 			</div>
 	</div>
 </body>
-</html>
+</html
