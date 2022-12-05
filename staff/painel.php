4b7f3bd88f83ef8c494b1c0b4626eb5127156d88
Matheus Viana <matheusvcv@yahoo.com.br>
2022-12-05 15:34:17 -0300

    Adicionado NavBar

--- staff/painel.php
+++ staff/painel.php
@@ -12,6 +12,32 @@
 	<title>Aluguel de veículos</title>
 </head>
 <body>
+	<!-- Início do NavBar -->
+
+	<nav class="navbar navbar-expand-lg navbar-dark fundo_preto fixed-top">
+	  <div class="container-fluid">
+	    <a class="navbar-brand" href="#"><img src="img/cosvip.png"></a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarNav">
+	      <ul class="navbar-nav">
+	        <li class="nav-item">
+	          <a class="nav-link active" aria-current="page" href="#">Início</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="experiencias-escritas.php">Experiências Escritas</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="costaviana-enterprise.php">CostaViana Enterprise</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="pagina-desenvolvedor.php">Página do Desenvolvedor</a>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
  	<br>
  	<div id="faixa">
  		<div id="sair">
