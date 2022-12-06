9d31d306ffce3d6f9ce3128b636859d1a79b3763
Matheus Viana <matheusvcv@yahoo.com.br>
2022-12-06 16:36:27 -0300

    Alterado link simples para card mais bem elaborado

--- staff/painel.php
+++ staff/painel.php
@@ -54,6 +54,14 @@
  		
  		<br>
 
+ 	<article class="card borda-cor-especial card-largura p-0 m-4 col-12 col-md-4">
+	    <div class="card-body">
+	      <h5 class="card-title">Cadastrar um novo Cliente</h5>
+	      <p class="card-text">Clique no botão abaixo para criar um cadastro para um novo cliente.</p>
+	      <a href="admin/cadastro.php" class="btn botao-cor-especial">Clique aqui</a>
+	    </div>
+    </article>
+
  		<div id="logo">
 
 	 		<p>
